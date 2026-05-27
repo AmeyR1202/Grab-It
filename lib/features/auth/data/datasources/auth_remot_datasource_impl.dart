84e063cf75ff948bc16754ffa700672968decb39
AmeyR1202 <ameyrakhe01@gmail.com>
2026-05-27 16:58:06 +0530

    Added the icon for the app

--- lib/features/auth/data/datasources/auth_remot_datasource_impl.dart
+++ lib/features/auth/data/datasources/auth_remot_datasource_impl.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:grab_it/core/errors/exceptions.dart';
